af8e79493ea84b96469646a9e7e99b4e93d6b518
Riipah <riipah@vocadb.net>
2022-08-11 22:07:34 +0300

    basic interface

---
+++ StringLocalizerWithCulture/IStringLocalizerWithCultureFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Localization;
+using System;
+using System.Globalization;
+
+namespace StringLocalizerWithCulture
+{
+    public interface IStringLocalizerWithCultureFactory
+    {
+        IStringLocalizer Create(Type type, CultureInfo culture);
+    }
+
+    public class StringLocalizerWithCultureFactory : IStringLocalizerWithCultureFactory
+    {
+        public IStringLocalizer Create(Type type, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StringLocalizerWithCulture/StringLocalizerWithCulture.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Resources;
+
+namespace StringLocalizerWithCulture
+{
+    internal class StringLocalizerWithCulture : IStringLocalizer
+    {
+
+        private readonly CultureInfo _culture;
+        private readonly ResourceManager _resources;
+
+        public LocalizedString this[string name] => new LocalizedString(name, _resources.GetString(name, _culture));
+
+        public LocalizedString this[string name, params object[] arguments]
+            => new LocalizedString(name, _resources.GetString(name, _culture));
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
